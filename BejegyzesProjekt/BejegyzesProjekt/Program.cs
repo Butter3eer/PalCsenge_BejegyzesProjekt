e599b3ab03998b34d6ec00e5baed2aed1dc3e726
Butter3eer <palcsenge2015@gmail.com>
2023-09-28 09:30:16 +0200

    2. feladatrész folytatása
    "B" feladatrész befejezése, melyben kivételkezeléssel kezelt input után tetszőleges mennyiségű bejegyzés kerül hozzáadásra a bejegyzesek listához.

--- BejegyzesProjekt/BejegyzesProjekt/Program.cs
+++ BejegyzesProjekt/BejegyzesProjekt/Program.cs
@@ -10,10 +10,33 @@ namespace BejegyzesProjekt
     {
         static void Main(string[] args)
         {
+            
         }
 
         static List<Bejegyzes> bejegyzesek = new List<Bejegyzes>() { new Bejegyzes("Nagy Pál", "Az őszi kecske kalandjait elmesélő könyv."), new Bejegyzes("Kis Pál", "A kis kecske kalandjait elmesélő regény.") };
 
-        
+        static void F2A()
+        {
+            int number;
+            Console.WriteLine("Darabszám: ");
+            if (int.TryParse(Console.ReadLine(), out number))
+            {
+                for (int i = 0; i < number; i++)
+                {
+                    Console.Write("Szerző: ");
+                    string szerzo = Console.ReadLine();
+
+                    Console.Write("\nTartalom: ");
+                    string tartalom = Console.ReadLine();
+
+                    bejegyzesek.Add(new Bejegyzes(szerzo, tartalom));
+                }
+            }
+            else
+            {
+                Console.WriteLine("Hibás input! Csak egész számot adhat meg!");
+                F2A();
+            }
+        }
     }
 }
