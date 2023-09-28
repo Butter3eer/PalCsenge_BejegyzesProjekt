f178acf818f9984b3bc53a187913ccc15c529ac0
Butter3eer <palcsenge2015@gmail.com>
2023-09-28 09:15:16 +0200

    2. feladatrész elkezdése
    "A" feladatrész elkészítése létrehozva egy Bejegyzes típusú listát már 2 db elemmel benne.

--- BejegyzesProjekt/BejegyzesProjekt/Program.cs
+++ BejegyzesProjekt/BejegyzesProjekt/Program.cs
@@ -11,5 +11,9 @@ namespace BejegyzesProjekt
         static void Main(string[] args)
         {
         }
+
+        static List<Bejegyzes> bejegyzesek = new List<Bejegyzes>() { new Bejegyzes("Nagy Pál", "Az őszi kecske kalandjait elmesélő könyv."), new Bejegyzes("Kis Pál", "A kis kecske kalandjait elmesélő regény.") };
+
+        
     }
 }
