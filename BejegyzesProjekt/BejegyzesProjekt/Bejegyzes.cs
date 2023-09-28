42d0e8416568336ac75819a35c17ce7f8da75f8d
Butter3eer <palcsenge2015@gmail.com>
2023-09-28 08:43:17 +0200

    A feladatrész befejezése
    Bejegyzes class adattagjainak, konstruktorának felépítése, Like() metódus létrehozása

--- BejegyzesProjekt/BejegyzesProjekt/Bejegyzes.cs
+++ BejegyzesProjekt/BejegyzesProjekt/Bejegyzes.cs
@@ -8,5 +8,31 @@ namespace BejegyzesProjekt
 {
     internal class Bejegyzes
     {
+        private string szerzo;
+        private string tartalom;
+        private int likeok;
+        private DateTime letrejott;
+        private DateTime szerkesztve;
+
+        public Bejegyzes(string szerzo, string tartalom)
+        {
+            this.szerzo = szerzo;
+            this.tartalom = tartalom;
+            this.likeok = 0;
+            this.letrejott = DateTime.Now;
+            this.szerkesztve = DateTime.Now;
+
+        }
+
+        public string Szerzo { get => szerzo; set => szerzo = value; }
+        public string Tartalom {get => tartalom; set {tartalom = value; szerkesztve = DateTime.Now;}}
+        public int Likeok { get => likeok; set => likeok = value; }
+        public DateTime Letrejott { get => letrejott; set => letrejott = value; }
+        public DateTime Szerkesztve { get => szerkesztve; set => szerkesztve = value; }
+
+        public void Like()
+        {
+            likeok++;
+        }
     }
 }
